a5e9dbf7027be21980bfd29ae891c0931fe1a3dc
PauloS5 <paulosocratesdesouza@gmail.com>
2026-03-14 23:54:58 -0500

    feat: implementa o componente de Formação

---
+++ src/components/main-components/EducationComponent.vue
@@ -0,0 +1,21 @@
+<script setup>
+  import data from '@/assets/data.json';
+</script>
+
+
+<template>
+    <h2>Formação</h2>
+
+    <ul v-for="course in data.education">
+        <li>
+            <h3>{{ course.title }}</h3>
+            <p>{{ course.institution }}</p>
+            <p>{{ course.description }}</p>
+            <p v-if="course.start_year && course.end_year">{{ course.start_year }} - {{ course.end_year }}</p>
+            <p>{{ course.duration }}</p>
+            <ul v-for="hab in course.habilities">
+                <li>{{ hab }}</li>
+            </ul>
+        </li>
+    </ul>
+</template>
