4d92afa4ee7b51a8476b233298f99298613f27ec
PauloS5 <paulosocratesdesouza@gmail.com>
2026-03-14 23:57:31 -0500

    fiz: atualiza a rota principal

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import MainView from '../views/MainView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: '/', name: 'home', component: HomeView }
+    { path: '/', name: 'home', component: MainView }
   ],
 })
 
