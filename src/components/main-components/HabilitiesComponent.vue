4ccbd94fa2eac4719cb1e77bca6745d3668a5d8e
PauloS5 <paulosocratesdesouza@gmail.com>
2026-03-14 23:55:29 -0500

    feat: implementa o componente de Habilidades

---
+++ src/components/main-components/HabilitiesComponent.vue
@@ -0,0 +1,18 @@
+<script setup>
+  import data from '@/assets/data.json';
+</script>
+
+<template>
+    <div>
+        <h2>Habilidades</h2>
+        <p>
+            Tecnologias que utilizo diariamente e domino.
+        </p>
+    </div>
+    
+    <div>
+        <ul v-for="tech in data.technologies">
+            <li>{{ tech }}</li>
+        </ul>
+    </div>
+</template>
