3456ca98e56190fce47024933e00661b3b2bdd29
PauloS5 <paulosocratesdesouza@gmail.com>
2026-03-14 23:55:13 -0500

    feat: implementa o componente de Experiência

---
+++ src/components/main-components/ExperienceComponent.vue
@@ -0,0 +1,18 @@
+<script setup>
+  import data from '@/assets/data.json';
+</script>
+
+<template>
+    <h2>Experiências</h2>
+
+    <ul v-for="exp in data.experiences">
+        <li>
+            <h3>{{ exp.title }}</h3>
+            <p>{{ exp.stack_holder }}</p>
+            <p>{{ exp.description }}</p>
+            <ul v-for="hab in exp.habilities">
+                <li>{{ hab }}</li>
+            </ul>
+        </li>
+    </ul>
+</template>
